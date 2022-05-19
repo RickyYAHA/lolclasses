03700ef7c786e08c57679437e6318b13a6c00298
RickyYAHA <100943801+RickyYAHA@users.noreply.github.com>
2022-05-19 20:17:57 +0200

    Add files via upload

---
+++ lol/ConsoleApp1/ConsoleApp1/Pickaxe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolClasses
+{
+    /*
+     * klasa opisująca konkretny rodzaj itemu w sklepie
+     */
+    public class Pickaxe : Item
+    {
+        //deprecated???
+        //public int bonusHP = 150;
+        public Pickaxe()
+        {
+            //ustalamy nazwe itemu
+            name = "Pickaxe";
+            // jego cene
+            price = 875;
+            //i atrybuty
+            AddStat(Stat.AD, 25);
+        }
+    }
+}
---
+++ lol/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using LolClasses;
+
+//stwórz nowego champa - garena
+Garen g = new Garen();
+//opdal emotke dla garena
+g.ctrl3();
+
+//stworz instacje przedmiotu - rubycrystal
+RubyCrystal rb = new RubyCrystal();
+Pickaxe px = new Pickaxe();
+//sprawdz ile ma hp garen
+g.showHP();
+g.showAD();
+//kup mu item
+g.buyItem(rb);
+//sprawdz ile ma teraz hp
+g.showHP();
+//kup mu drugi item
+g.buyItem(rb);
+g.buyItem(px);
+//sprawdz ile ma teraz hp
+g.showHP();
+g.showAD();
+
+
+
+
+
+
